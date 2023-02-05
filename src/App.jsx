d889425549cdbeded9afcc962ca5e0df032c2a0b
junjiecheo12 <wkcheojj@gmail.com>
2023-02-05 15:51:54 +0800

    add projects

--- src/App.jsx
+++ src/App.jsx
@@ -27,6 +27,21 @@ function App() {
         </a>
         <a href="https://github.com/applecheo">Github</a>
       </div>
+      <h2>My Projects:</h2>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-evenly",
+          marginTop: 32,
+        }}
+      >
+        <a href="https://vercel.com/applecheo/beams-e-commerce">
+          Beams E-Commerce
+        </a>
+        <a href="https://shoesial.vercel.app/">
+          ShoeSial <span> (in progress)</span>
+        </a>
+      </div>
     </>
   );
 }
